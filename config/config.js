9a9b6e284ca711e4face874269e912b859161e24
frillic <69489498+crowe828@users.noreply.github.com>
2020-10-21 18:36:09 -0400

    correct db name is alien_db

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ module.exports = {
   "development": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
-    "database": "aliens_db",
+    "database": "alien_db",
     "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
