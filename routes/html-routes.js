d47e46a1b7458cbfd077d3c404e8cda82279754d
katiec <katiecampbell21@outlook.com>
2020-10-21 12:52:29 -0400

    built form to submit sighting

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -21,6 +21,13 @@ module.exports = function(app) {
     res.render("login");
   });
 
+  app.get("/members/report", (req, res) => {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.render("report");
+    }
+  });
+
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
