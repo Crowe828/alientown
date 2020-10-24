9c8c5365f19e151c6b78b466edc7667f923452c9
frillic <69489498+crowe828@users.noreply.github.com>
2020-10-24 18:53:53 -0400

    Map now displays multiple data points

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,8 @@
-// Google Maps variables
+// Variables for first two functions
 let pos;
 let map;
 let bounds;
 let infoWindow;
-let currentInfoWindow;
-let service;
-
-// eslint-disable-next-line no-var
-var geocoder;
 
 // eslint-disable-next-line no-unused-vars
 function initMap() {
@@ -16,8 +11,7 @@ function initMap() {
   infoWindow = new google.maps.InfoWindow();
   currentInfoWindow = infoWindow;
   infoPane = document.getElementById("panel");
-  //geocoder
-  geocoder = new google.maps.Geocoder();
+
   // Geolocation
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
@@ -28,29 +22,24 @@ function initMap() {
         };
         map = new google.maps.Map(document.getElementById("map"), {
           center: pos,
-          zoom: 5
+          zoom: 2
         });
 
         bounds.extend(pos);
         infoWindow.setPosition(pos);
-        infoWindow.setContent("Location found.");
+        infoWindow.setContent("YOU ARE HERE");
         infoWindow.open(map);
         map.setCenter(pos);
-
-        // Display places near the user
-        getNearbyPlaces(pos);
-        // Call the function
-        getData();
       },
       () => {
         // Browser supports geolocation, but user denies permission
         handleLocationError(true, infoWindow);
       }
     );
-  } else {
-    // Browser doesn't support geolocation
-    handleLocationError(false, infoWindow);
   }
+
+  // Retrieve the data from the db
+  getData();
 }
 
 // If there is a geolocation error
@@ -59,7 +48,7 @@ function handleLocationError(browserHasGeolocation, infoWindow) {
   pos = { lat: 28.5492, lng: -81.3798 };
   map = new google.maps.Map(document.getElementById("map"), {
     center: pos,
-    zoom: 5,
+    zoom: 2
   });
 
   // If there is an error, let the user know
@@ -75,78 +64,12 @@ function handleLocationError(browserHasGeolocation, infoWindow) {
 
   // Display places in Orlando, Florida
   getNearbyPlaces(pos);
-  // Call the function
-  getData();
-}
-
-// Search for nearby palces
-function getNearbyPlaces(position) {
-  const request = {
-    location: position,
-    rankBy: google.maps.places.RankBy.DISTANCE,
-    keyword: "museum",
-  };
-
-  service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, nearbyCallback);
-}
-
-// Handle the results of the search
-function nearbyCallback(results, status) {
-  if (status === google.maps.places.PlacesServiceStatus.OK) {
-    createMarkers(results);
-  }
-}
-
-// Set a marker at each location
-function createMarkers(places) {
-  places.forEach((place) => {
-    const marker = new google.maps.Marker({
-      position: place.geometry.location,
-      map: map,
-      title: place.name,
-    });
-
-    // Add click listener to each marker
-    google.maps.event.addListener(marker, "click", () => {
-      const request = {
-        placeId: place.place_id,
-        fields: ["name"],
-      };
-      // Only fetch the details of a place when the user clicks on a marker
-      service.getDetails(request, (placeResult, status) => {
-        showDetails(placeResult, marker, status);
-      });
-    });
-    // Adjust the map bounds to include the location of this marker
-    bounds.extend(place.geometry.location);
-  });
-  // Once all the markers have been placed, adjust the bounds of the map to show all the markers within the visible area.
-  map.fitBounds(bounds);
-}
-
-// Displays details above the marker
-function showDetails(placeResult, marker, status) {
-  if (status === google.maps.places.PlacesServiceStatus.OK) {
-    const placeInfowindow = new google.maps.InfoWindow();
-    placeInfowindow.setContent(
-      "<div><strong>" + placeResult.name + "</strong></div>"
-    );
-    placeInfowindow.open(marker.map, marker);
-    currentInfoWindow.close();
-    currentInfoWindow = placeInfowindow;
-    showPanel(placeResult);
-  } else {
-    console.log("showDetails failed: " + status);
-  }
 }
 
 // Pull all of the info from the database
 function getData() {
-  $.get("/api/all/", (data) => {
+  $.get("/api/all/", data => {
     if (data) {
-      // Check to make sure it pulled correctly
-      console.log(data);
       // Empty array to hold all of the city names
       const cities = [];
       for (let i = 0; i < data.length; i++) {
@@ -157,19 +80,30 @@ function getData() {
       console.log(cities);
 
       // Loop over all cities and get lat/lon
-      for (i = 0; i < cities.length; i++) {
+      for (let i = 0; i < cities.length; i++) {
         // Use geocoder to get cities' lat/lon
-        geocoder.geocode({ address: cities[i] }, (results, status) => {
-          if (status === google.maps.GeocoderStatus.OK) {
-            newAddress = results[0].geometry.location;
-            const latlng = new google.maps.LatLng(
-              parseFloat(newAddress.lat()),
-              parseFloat(newAddress.lng())
-            );
-            console.log(latlng);
-            // gmarkers.push(createMarker(latlng, content[i]));
+        const geocoder = new google.maps.Geocoder();
+        const address = cities[i];
+        geocoder.geocode(
+          {
+            address: address
+          },
+          (results, status) => {
+            if (status === google.maps.GeocoderStatus.OK) {
+              // lat/lng variables to store the coordinates generated by Google Maps
+              const lat = results[0].geometry.location.lat();
+              const lng = results[0].geometry.location.lng();
+              // Log the data points
+              console.log("lat: " + lat, "lng: " + lng);
+              // Create a marker for each set of coordinates
+              new google.maps.Marker({
+                position: { lat: lat, lng: lng },
+                map,
+                title: "ALIENS!"
+              });
+            }
           }
-        });
+        );
       }
     }
   });
